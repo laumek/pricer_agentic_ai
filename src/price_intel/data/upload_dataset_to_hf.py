1ec9f8aa0dd7007b70f76a2fd1af3b8bd90cdb26
laumek <laureenmekuilm@gmail.com>
2025-11-28 12:15:05 +0000

    Upload dataset to HF

---
+++ src/price_intel/data/upload_dataset_to_hf.py
@@ -0,0 +1,34 @@
+# imports
+
+import pickle
+from datasets import Dataset, DatasetDict
+from price_intel.data.env_setup import setup_environment, login_huggingface
+# from dotenv import load_dotenv
+# from huggingface_hub import login
+
+token = setup_environment()
+login_huggingface(token)
+
+
+with open("amazon_items_train.pkl", "rb") as f:
+    train = pickle.load(f)
+
+with open("amazon_items_test.pkl", "rb") as f:
+    test = pickle.load(f)
+
+train_prompts = [item.prompt for item in train]
+train_prices = [item.price for item in train]
+test_prompts = [item.test_prompt() for item in test]
+test_prices = [item.price for item in test]
+
+train_dataset = Dataset.from_dict({"text": train_prompts, "price": train_prices})
+test_dataset = Dataset.from_dict({"text": test_prompts, "price": test_prices})
+
+dataset = DatasetDict({
+    "train": train_dataset,
+    "test": test_dataset
+})
+
+DATASET_NAME = "laureen-ai/pricer-data"
+dataset.push_to_hub(DATASET_NAME, private = True)
+
